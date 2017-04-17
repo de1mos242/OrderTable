65e856d3217f909eea3a2d08fc05dca947d72fa0
de1mos <de1m0s242@gmail.com>
2017-04-18 02:44:25 +0700

    python skeleton

---
+++ server/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='OrderTable',
+    version='1.0',
+    packages=[''],
+    url='ordertable.de1mos.net',
+    license='MIT',
+    author='De1mos',
+    author_email='de1m0s242@gmail.com',
+    description='Order table web app'
+)
